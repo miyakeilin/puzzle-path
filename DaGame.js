4c162dbd6f53b3fac739b4d80565b76aa35d424d
Thain <liam.chung@mail.mcgill.ca>
2019-11-09 01:28:28 -0500

    begin implementing 2 player model

--- DaGame.js
+++ DaGame.js
@@ -1,8 +1,9 @@
-var myGuy = [1,2]
+var P1 = [1,0]
+var P2 = [2,0]
 const dim = 3
 
-function move(dir){
-  return tile[myGuy[0]][myGuy[1]][dir];
+function movePlayer(dir, p){
+  return tile[p[0]][p[1]][dir];
 }
 
 var tile = new Array(dim);
@@ -30,6 +31,15 @@ for(var i = 0; i < dim; i++)
 //   }
 // }
 
+
+var tile = new Array(dim);
+for(var i = 0; i < dim; i++) {
+  tile[i] = new Array(dim);
+  for(var j = 0; j < dim; j++){
+    tile[i][j] = {left:[i,(j+(dim-1))%dim], right:[i,(j+1)%dim], up:[(i+(dim-1))%dim,j], down:[(i+1)%dim,j]}
+  }
+}
+
 tile[0][0] = {left:[0,2], right:[0,0], up:[2,0], down:[1,0]}
 tile[0][1] = {left:[0,1], right:[0,1], up:[1,2], down:[1,1]}
 tile[0][2] = {left:[0,2], right:[0,2], up:[0,0], down:[1,2]}
@@ -40,4 +50,6 @@ tile[2][0] = {left:[2,2], right:[2,0], up:[1,0], down:[0,0]}
 tile[2][1] = {left:[2,1], right:[2,2], up:[1,1], down:[2,1]}
 tile[2][2] = {left:[2,1], right:[2,0], up:[2,2], down:[2,2]}
 
-myGuy = move("up");
+[P1,P2] = move("left", P1, P2)
+console.log(P2)
+//alert([P1, P2])
